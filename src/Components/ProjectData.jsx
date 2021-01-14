00480a05f4f9d152d871debe8c69ed3729585af5
ankit <erakverma97@gmail.com>
2021-01-14 13:30:51 +0530

    resume

---
+++ src/Components/ProjectData.jsx
@@ -0,0 +1,22 @@
+const ProjectData =[
+    {
+        key:"1",
+        projectName : "Management-cum-shopping software",
+        link : "https://github.com/akvblue19/Management-cum-shopping-software",
+        linkName :"https://github.com/akvblue19/Management-cum-shopping-software",
+        desc : "Its a simple ",
+        TechUsed : "React,  Spring Boot, Hibernate, OAuth2",
+    },
+
+    {
+        key:"2",
+        projectName : "TechInfo Android App",
+        link : "https://github.com/akvblue19/TechInfo",
+        linkName : "https://github.com/akvblue19/TechInfo",
+        desc : "Its a simple Android application",
+        TechUsed : "Android",
+    },
+
+]
+
+export default ProjectData;
